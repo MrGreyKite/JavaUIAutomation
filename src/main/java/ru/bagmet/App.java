85b339c2afb7778b21a2c9b4c67447b4db677779
Рудольф Багмет <nolde3@yandex.ru>
2022-03-24 20:40:58 +0400

    Initial commit

---
+++ src/main/java/ru/bagmet/App.java
@@ -0,0 +1,13 @@
+package ru.bagmet;
+
+/**
+ * Hello world!
+ * author: Rudolf Bagmet
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! It's app for Java UI Automation course" );
+    }
+}
